4a2adc628809e3861053ff3e2866792722685c2a
Boaz E.C Theodoris <92237004+eddo4life@users.noreply.github.com>
2025-03-12 10:48:21 -0400

    commit1.2

--- chapter1/exercices.py
+++ chapter1/exercices.py
@@ -19,3 +19,13 @@ print(f"The maximum number of steps it would take to search through a list of {n
 # The maximum number of steps it would take to search through a list of 128 names using binary search is 7.
 
 
+# Suppose you double the size of the list
+num_names_doubled = num_names * 2
+
+# Calculate the maximum number of steps for the doubled list
+max_steps_doubled = max_steps_binary_search(num_names_doubled)
+
+print(f"The maximum number of steps it would take to search through a list of {num_names_doubled} names using binary search is {max_steps_doubled}.")
+
+# Output
+# The maximum number of steps it would take to search through a list of 256 names using binary search is 8.
